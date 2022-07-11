2619e498f2a08ce5a84287aa5b65e4970100748c
Jessie Luo <jluo30@jhu.edu>
2022-07-11 00:06:54 -0700

    feat: add second view without storyboard

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = "\(String(format: "%.2f", sender.value))m"
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = "\(Int(sender.value))Kg"
+    }
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+        print(bmi)
+        
+        let secondVC = SecondViewController()
+        
+        self.present(secondVC, animated: true, completion: nil)
+    }
+    
 
 }
 
